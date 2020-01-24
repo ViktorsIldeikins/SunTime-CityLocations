446f6dcacf16ac0e3a73a700b0e1ad91a441016c
viktors.ildeikins <V.Ildeikins@gmail.com>
2020-01-24 15:09:17 +0200

    Accept request from other origin; Clean up imports

--- src/main/java/com/daytime/citylocations/CityLocationsApplication.java
+++ src/main/java/com/daytime/citylocations/CityLocationsApplication.java
@@ -1,13 +1,7 @@
 package com.daytime.citylocations;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.List;
-
-import static java.util.Arrays.asList;
 
 @SpringBootApplication
 public class CityLocationsApplication {
